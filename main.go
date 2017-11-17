a5497fe51ece5fb24327e661aee3b3785b445feb
fschlosser <fschlosser@users.noreply.github.com>
2017-11-17 23:40:55 +0100

    Fix code indents

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"lang-term-counter/src/wiki"
+    "fmt"
+    "net/http"
+    "lang-term-counter/src/wiki"
 )
 
 func main() {
 
-	url := "https://en.wikipedia.org/wiki/Hans_Mork"
+    url := "https://en.wikipedia.org/wiki/Hans_Mork"
 
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("Error: unable to load content of url '%s'\n", url)
-		return
-	}
+    response, err := http.Get(url)
+    if err != nil {
+        fmt.Printf("Error: unable to load content of url '%s'\n", url)
+        return
+    }
 
-	parser := wiki.NewContentParser()
-	content := parser.Parse(response.Body)
+    parser := wiki.NewContentParser()
+    content := parser.Parse(response.Body)
 
-	fmt.Printf("Content:\n%s\n", content)
+    fmt.Printf("Content:\n%s\n", content)
 }
--- src/wiki/content_parser.go
+++ src/wiki/content_parser.go
@@ -1,102 +1,102 @@
 package wiki
 
 import (
-	"io"
-	"golang.org/x/net/html"
-	"bytes"
+    "io"
+    "golang.org/x/net/html"
+    "bytes"
 )
 
 const (
-	contentDivIdValue = "mw-content-text"
+    contentDivIdValue = "mw-content-text"
 )
 
 type contentParser struct {
-	shouldPrintTextOfToken bool
-	divCount               int
-	buffer                 bytes.Buffer
-	handlers               map[html.TokenType]func(token html.Token)(finish bool)
+    shouldPrintTextOfToken bool
+    divCount               int
+    buffer                 bytes.Buffer
+    handlers               map[html.TokenType]func(token html.Token)(finish bool)
 }
 
 func NewContentParser() *contentParser {
-	parser := &contentParser{
-		shouldPrintTextOfToken: false,
-		divCount:               0,
-		buffer:                 bytes.Buffer{},
-		handlers:               make(map[html.TokenType]func(token html.Token)(finish bool)),
-	}
-	parser.handlers[html.StartTagToken] = parser.handleStartTagToken
-	parser.handlers[html.EndTagToken] = parser.handleEndTagToken
-	parser.handlers[html.TextToken] = parser.handleTextToken
-	parser.handlers[html.ErrorToken] = parser.handleErrorToken
-	return parser
+    parser := &contentParser{
+        shouldPrintTextOfToken: false,
+        divCount:               0,
+        buffer:                 bytes.Buffer{},
+        handlers:               make(map[html.TokenType]func(token html.Token)(finish bool)),
+    }
+    parser.handlers[html.StartTagToken] = parser.handleStartTagToken
+    parser.handlers[html.EndTagToken] = parser.handleEndTagToken
+    parser.handlers[html.TextToken] = parser.handleTextToken
+    parser.handlers[html.ErrorToken] = parser.handleErrorToken
+    return parser
 }
 
 func (p *contentParser) Parse(reader io.Reader) string {
-	tokenizer := html.NewTokenizer(reader)
+    tokenizer := html.NewTokenizer(reader)
 
-	p.shouldPrintTextOfToken = false
-	p.divCount = 0
-	p.buffer = bytes.Buffer{}
+    p.shouldPrintTextOfToken = false
+    p.divCount = 0
+    p.buffer = bytes.Buffer{}
 
-	for {
-		if p.processToken(tokenizer.Next(), tokenizer.Token()) {
-			break
-		}
-	}
-	return p.buffer.String()
+    for {
+        if p.processToken(tokenizer.Next(), tokenizer.Token()) {
+            break
+        }
+    }
+    return p.buffer.String()
 }
 
 func (p *contentParser) processToken(tokenType html.TokenType, token html.Token) (finish bool) {
-	handler := p.handlers[tokenType]
-	if handler == nil {
-		return false
-	}
-	return handler(token)
+    handler := p.handlers[tokenType]
+    if handler == nil {
+        return false
+    }
+    return handler(token)
 }
 
 func (p *contentParser) handleStartTagToken(token html.Token) (finish bool) {
-	// to handle divs within the content div
-	if token.Data == "div" && p.divCount > 0 {
-		p.divCount++
-	} else
-	if token.Data == "p" {
-		p.shouldPrintTextOfToken = p.divCount > 0
-	} else
-	if isTokenTheContentDiv(token) {
-		p.divCount++
-	}
-	return false
+    // to handle divs within the content div
+    if token.Data == "div" && p.divCount > 0 {
+        p.divCount++
+    } else
+    if token.Data == "p" {
+        p.shouldPrintTextOfToken = p.divCount > 0
+    } else
+    if isTokenTheContentDiv(token) {
+        p.divCount++
+    }
+    return false
 }
 
 func (p *contentParser) handleEndTagToken(token html.Token) (finish bool) {
-	if token.Data == "div" && p.divCount > 0 {
-		p.divCount--
-	} else
-	if token.Data == "p" {
-		p.shouldPrintTextOfToken = false
-	}
-	return false
+    if token.Data == "div" && p.divCount > 0 {
+        p.divCount--
+    } else
+    if token.Data == "p" {
+        p.shouldPrintTextOfToken = false
+    }
+    return false
 }
 
 func (p *contentParser) handleTextToken(token html.Token) (finish bool) {
-	if p.shouldPrintTextOfToken {
-		p.buffer.WriteString(token.Data)
-	}
-	return false
+    if p.shouldPrintTextOfToken {
+        p.buffer.WriteString(token.Data)
+    }
+    return false
 }
 
 func (p *contentParser) handleErrorToken(token html.Token) (finish bool) {
-	return true // EOF
+    return true // EOF
 }
 
 func isTokenTheContentDiv(token html.Token) bool {
-	if token.Data != "div" {
-		return false
-	}
-	for _, attribute := range token.Attr {
-		if attribute.Key == "id" && attribute.Val == contentDivIdValue {
-			return true
-		}
-	}
-	return false
+    if token.Data != "div" {
+        return false
+    }
+    for _, attribute := range token.Attr {
+        if attribute.Key == "id" && attribute.Val == contentDivIdValue {
+            return true
+        }
+    }
+    return false
 }
